334d8b7a70bd67470516f8e20d8f577c008436b7
vasyakov <vasyakov94@gmail.com>
2021-02-22 20:14:44 +0300

    Add exercise1

---
+++ Lesson 5/src/exercise1/MyMath.java
@@ -0,0 +1,17 @@
+package exercise1;
+
+public class MyMath {
+
+    private static double result = 1;
+
+    public static double power(double val, int power) {
+        if (power == 0) return result;
+        if (power > 0) {
+            result *= val;
+            return power(val, --power);
+        } else {
+            result /= val;
+            return power(val, ++power);
+        }
+    }
+}
---
+++ Lesson 5/src/exercise1/PowTest.java
@@ -0,0 +1,7 @@
+package exercise1;
+
+public class PowTest {
+    public static void main(String[] args) {
+        System.out.println(MyMath.power(2, -2));
+    }
+}
